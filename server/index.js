cc504cb5a737b76e6fbd151f5cc816d0f2c925b8
Rich B <richardbaker1993@gmail.com>
2023-11-09 11:21:49 +0000

    feat: skeleton server runnning

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const router = require('./router');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server rolling on ${port}`);
+});
