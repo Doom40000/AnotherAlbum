1cddb00e4c74d5248684d7046ae8218fa30b7702
Rich B <richardbaker1993@gmail.com>
2023-11-09 12:18:39 +0000

    Feat: displaying one album

---
+++ server/models/db.js
@@ -0,0 +1,11 @@
+const albumArtPath = '/Users/Rich/coding/Codeworks/Projects/Solo Project/AnotherAlbum/mockImages/';
+
+const mockData = [
+  {
+    id: 1,
+    artist: 'Tim Hecker',
+    cover: `${albumArtPath}_ravedeath1972_3plv.jpg`
+  }
+]
+
+module.exports = mockData;
--- server/router.js
+++ server/router.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const mockData = require('./models/db');
 
 
 router.get('/', (req, res) => {
   res.send('Hello world!');
+});
+
+router.get('/album', (req, res) => {
+  res.sendFile(mockData[0].cover)
 })
 
 module.exports = router;
