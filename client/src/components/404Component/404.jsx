de82bc1e64ee33a1b727d18d2322355e7506552f
Libertex13 <nachoplanas.np@gmail.com>
2023-11-16 12:28:02 +0100

    Vitest setup

--- client/src/components/404Component/404.jsx
+++ client/src/components/404Component/404.jsx
@@ -1,12 +1,12 @@
+//client\src\components\404Component\404.jsx
 import "./404.css";
 
-
-export default function FourZeroFour () {
+export default function FourZeroFour() {
   return (
     <div className="fzf">
       <h1>Oops!</h1>
       <h2>404: Page not found. ☠️</h2>
       <h3>Time to turn around. ↩️</h3>
     </div>
-  )
+  );
 }
